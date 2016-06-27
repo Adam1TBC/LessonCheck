3ed99b4cc1ddd0d77f303aefa38c8339d34e0c52
Your Name <you@example.com>
2016-06-27 14:34:25 +0300

    pods

--- Pods/Target Support Files/HamburgerButton/HamburgerButton-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HamburgerButton : NSObject
-@end
-@implementation PodsDummy_HamburgerButton
-@end
--- Pods/Target Support Files/HamburgerMenu/HamburgerMenu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HamburgerMenu : NSObject
-@end
-@implementation PodsDummy_HamburgerMenu
-@end
